6af7b4a9271f86d6873cf6da1d553df468b661ef
Raki <46581776+mdrakiburrahman@users.noreply.github.com>
2023-03-27 06:45:21 -0700

    Dual infinite loops

--- Program.cs
+++ Program.cs
@@ -38,96 +38,117 @@ namespace UserArrP
 
         private static async Task RunAsync()
         {
-            AuthenticationConfig config = AuthenticationConfig.ReadFromJsonFile("appsettings.json");
-
-            // The application is a confidential client application
+            // This will keep on getting new URLs upon expiry, and trying again
             //
-            IConfidentialClientApplication app;
-
-            app = ConfidentialClientApplicationBuilder.Create(config.ClientId)
-                    .WithClientSecret(config.ClientSecret)
-                    .WithAuthority(new Uri(config.Authority))
-                    .WithExperimentalFeatures() // for PoP
-                    .Build();
-
-            app = ConfidentialClientApplicationBuilder.Create(config.ClientId)
-                .WithClientSecret(config.ClientSecret)
-                .WithAuthority(new Uri(config.Authority))
-                .WithExperimentalFeatures() // for PoP
-                .Build();
-
-            app.AddInMemoryTokenCache();
-
-            // This is basically the Client ID of the specific Arc Server, visible in AAD:
-            //
-            // - https://ms.portal.azure.com/#view/Microsoft_AAD_IAM/ManagedAppMenuBlade/~/Overview/objectId/c579b537-d28b-491a-98b0-fccd193c2d05/appId/5fa47195-e890-485e-a90c-3d417cfcb1e2
-            //   e.g. "5fa47195-e890-485e-a90c-3d417cfcb1e2/.default"
-            //   
-            string[] scopes = new string[] { config.ArcServerScope };
-            string popUri = $"{config.ArceeApiBaseAddress}";
-
-            AuthenticationResult result = null;
-            try
+            while (true)
             {
-                result = await app.AcquireTokenForClient(scopes)
-                    .WithProofOfPossession(new PoPAuthenticationConfiguration(new Uri(popUri)) { HttpMethod = HttpMethod.Get })
-                    .ExecuteAsync();
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("Token acquired \n");
-                Console.ResetColor();
-            }
-            catch (MsalServiceException ex)
-            {
-                // Invalid scope. The scope has to be of the form "https://resourceurl/.default"
-                // Mitigation: change the scope to be as expected
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine(ex.Message);
-                Console.ResetColor();
-            }
-
-            if (result != null)
-            {
-                var httpClient = new HttpClient();
-                var apiCaller = new ProtectedApiCallHelper(httpClient);
-
-                // Benchmark
-                //
-                double total_qps = 0;
-                int num_queries = 0;
-                var start_time = DateTime.Now;
-
-                while (true)
+                try
                 {
-                    // Send GET request to the API endpoint and get the JSON payload
-                    var apiResult = await apiCaller.CallWebApiAndProcessResultASync(popUri, result);
+                    AuthenticationConfig config = AuthenticationConfig.ReadFromJsonFile("appsettings.json");
 
-                    // Calculate the elapsed time for each API call
-                    var elapsed_time = (DateTime.Now - start_time).TotalSeconds;
+                    // The application is a confidential client application
+                    //
+                    IConfidentialClientApplication app;
 
-                    // Calculate the queries per second (QPS) and update the rolling average
-                    var qps = 1 / elapsed_time;
-                    total_qps = (total_qps * num_queries + qps) / (num_queries + 1);
-                    num_queries += 1;
+                    app = ConfidentialClientApplicationBuilder.Create(config.ClientId)
+                            .WithClientSecret(config.ClientSecret)
+                            .WithAuthority(new Uri(config.Authority))
+                            .WithExperimentalFeatures() // for PoP
+                            .Build();
 
-                    // Print the rolling average QPS, server name, and server time
-                    JsonArray nodes = apiResult.AsArray();
-                    var statistics = $"Query: {num_queries}: Average QPS = {total_qps} queries/second";
+                    app = ConfidentialClientApplicationBuilder.Create(config.ClientId)
+                        .WithClientSecret(config.ClientSecret)
+                        .WithAuthority(new Uri(config.Authority))
+                        .WithExperimentalFeatures() // for PoP
+                        .Build();
 
-                    foreach (JsonObject aNode in nodes.ToArray().Cast<JsonObject>())
+                    app.AddInMemoryTokenCache();
+
+                    // This is basically the Client ID of the specific Arc Server, visible in AAD:
+                    //
+                    // - https://ms.portal.azure.com/#view/Microsoft_AAD_IAM/ManagedAppMenuBlade/~/Overview/objectId/c579b537-d28b-491a-98b0-fccd193c2d05/appId/5fa47195-e890-485e-a90c-3d417cfcb1e2
+                    //   e.g. "5fa47195-e890-485e-a90c-3d417cfcb1e2/.default"
+                    //   
+                    string[] scopes = new string[] { config.ArcServerScope };
+
+                    // This is the short lived Relay URL:
+                    //
+                    //  e.g. "https://68d3eb8a77c95a7acf97c4d61c0fe84e.c579b537-d28b-491a-98b0-fccd193c2d05.eastus.arc.waconazure.com:6443"
+                    //
+                    string popUri = $"{config.ArceeApiBaseAddress}";
+
+                    AuthenticationResult result = null;
+                    try
                     {
-                        foreach (var property in aNode.ToArray())
+                        result = await app.AcquireTokenForClient(scopes)
+                            .WithProofOfPossession(new PoPAuthenticationConfiguration(new Uri(popUri)) { HttpMethod = HttpMethod.Get })
+                            .ExecuteAsync();
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Token acquired \n");
+                        Console.ResetColor();
+                    }
+                    catch (MsalServiceException ex)
+                    {
+                        // Invalid scope. The scope has to be of the form "https://resourceurl/.default"
+                        // Mitigation: change the scope to be as expected
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine(ex.Message);
+                        Console.ResetColor();
+                    }
+
+                    if (result != null)
+                    {
+                        var httpClient = new HttpClient();
+                        var apiCaller = new ProtectedApiCallHelper(httpClient);
+
+                        // Benchmark
+                        //
+                        double total_qps = 0;
+                        int num_queries = 0;
+                        var start_time = DateTime.Now;
+
+                        // This will keep on looping until the Relay URL expires, and then throw
+                        //
+                        while (true)
                         {
-                            if (property.Key == "Server Name" || property.Key == "Server Time")
+                            // Send GET request to the API endpoint and get the JSON payload
+                            var apiResult = await apiCaller.CallWebApiAndProcessResultASync(popUri, result);
+
+                            // Calculate the elapsed time for each API call
+                            var elapsed_time = (DateTime.Now - start_time).TotalSeconds;
+
+                            // Calculate the queries per second (QPS) and update the rolling average
+                            var qps = 1 / elapsed_time;
+                            total_qps = (total_qps * num_queries + qps) / (num_queries + 1);
+                            num_queries += 1;
+
+                            // Print the rolling average QPS, server name, and server time
+                            JsonArray nodes = apiResult.AsArray();
+                            var statistics = $"Query: {num_queries}: Average QPS = {total_qps} queries/second";
+
+                            foreach (JsonObject aNode in nodes.ToArray().Cast<JsonObject>())
                             {
-                            
-                                statistics += $" | {property.Key}: {property.Value?.ToString()}";
+                                foreach (var property in aNode.ToArray())
+                                {
+                                    if (property.Key == "Server Name" || property.Key == "Server Time")
+                                    {
+
+                                        statistics += $" | {property.Key}: {property.Value?.ToString()}";
+                                    }
+                                }
                             }
+                            Console.WriteLine(statistics);
+
+                            // Reset the start time for the next API call
+                            start_time = DateTime.Now;
                         }
                     }
-                    Console.WriteLine(statistics);
-
-                    // Reset the start time for the next API call
-                    start_time = DateTime.Now;
+                }
+                catch (Exception ex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine(ex.Message);
+                    Console.ResetColor();
                 }
             }
         }
